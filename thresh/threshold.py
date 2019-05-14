47d7c4dc9749aaf4d3a77e8273affbc0eb057c90
chenyuanfeng421 <chenyuanfeng88@yeah.net>
2019-05-14 19:07:31 +0800

    change

--- thresh/threshold.py
+++ thresh/threshold.py
@@ -2,14 +2,14 @@ import cv2 as cv
 
 cv.namedWindow('Image', 0)
 # 预生成多个窗口供显示处理后图片
-img = cv.imread('image/66.png')
+img = cv.imread('image/test3.PNG')
 # 将需要处理的图片读入
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 print(cv.COLOR_BGR2GRAY)
 print(img)
 
-ret, img = cv.threshold(img, 127, 255, 4, img)
+ret, img = cv.threshold(img, 127, 255, cv.THRESH_TRUNC, img)
 # 处理图片
 print()
 
